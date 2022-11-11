f46d0608f90e342ffafcba0ef0fd7b9a63c20778
ITlearning <yo7504@naver.com>
2022-11-12 02:22:01 +0900

    Chore: 가로모드 고정

--- MiTamin/Source/System/AppDelegate.swift
+++ MiTamin/Source/System/AppDelegate.swift
@@ -81,5 +81,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        // 세로방향 고정
+        return UIInterfaceOrientationMask.portrait
+    }
 }
 
